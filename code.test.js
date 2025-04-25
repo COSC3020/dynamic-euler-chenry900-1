251baa2a4e11695dab20031d088fb339c7869038
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-04-25 17:35:26 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
+const EPSILON = 1e-10;
 
 assert(factorial(10) === 3628800);
 assert(e(0) === 1);
 assert(e(1) === 2.000000);
-assert(e(5) === 2.7166666667);
-assert(e(10) === 2.7182818011463845);
-assert(e(15) === 2.71828182845823);
-assert(e(20) === 2.7182818284590455);
+assert(Math.abs(e(5) - 2.7166666667) < EPSILON);
+assert(Math.abs(e(10) === 2.7182818011463845 < EPSILON);
+assert(Math.abs(e(15) === 2.71828182845823 < EPISLON);
+assert(Math.abs(e(20) === 2.7182818284590455 < EPISLON);
