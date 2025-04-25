01434847cbd9ec3dab66133a9cc33cce51a21786
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-04-25 17:26:18 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,8 +4,8 @@ eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(0) === 1);
-assert(e(1) === 2.71828182846);
-assert(e(5) === 13.5914091423);
+assert(e(1) === 2.000000);
+assert(e(5) === 2.271667);
 assert(e(10) === 2.7182818011463845);
-assert(e(15) === 40.7742274269);
-assert(e(20) === 54.3656365692);
+assert(e(15) === 2.718281);
+assert(e(20) === 2.718281);
